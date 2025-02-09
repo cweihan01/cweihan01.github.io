af7e6358ec13b8036bb966726cb729d1ea297f94
Wei Han <chuaweihan0101@gmail.com>
2025-02-08 22:34:34 -0800

    Remove starter code
    - Remove default files from project creation
    - Remove unused base styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
 import Home from './components/Home.vue';
 import Experience from './components/Experience.vue';
 import Projects from '@/components/Projects.vue';
@@ -10,18 +8,6 @@ import About from './components/About.vue';
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header> -->
-  
-  <!-- <main>
-    <TheWelcome />
-  </main> -->
-
   <main>
     <Home />
     <About />
--- src/main.js
+++ src/main.js
@@ -3,17 +3,11 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 
-/* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core';
-
-/* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-/* import specific icons */
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons';
-
-/* add icons to the library */
 library.add(fas, fab);
 
 createApp(App).component('fa-icon', FontAwesomeIcon).mount('#app');
